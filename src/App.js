2334f32f2a9537f1febd15e83bde130b1eb9bd6f
Jon Deibel <deibeljc@mail.uc.edu>
2016-02-06 17:25:46 -0500

    Hooked up react and firebase with rebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import Rebase from 're-base';
 
 export default class App extends Component {
-  render() {
-    return (
-      <h1>Hello, world.</h1>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.base = Rebase.createClass('https://context-prototype.firebaseio.com/');
+        this.state = {
+            name: ""
+        }
+    }
+
+    componentDidMount() {
+        this.base.syncState('name', {
+            context: this,
+            state: 'name'
+        })
+    }
+
+    render() {
+        return (
+            <h1>Hello, {this.state.name}</h1>
+        );
+    }
 }
