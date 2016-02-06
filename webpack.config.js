31d9badff183d3887a6bb1055aa9a460e584d2f2
Jon Deibel <deibeljc@mail.uc.edu>
2016-02-06 17:01:08 -0500

    Finishing up the environment

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,14 @@ module.exports = {
       test: /\.js$/,
       loaders: ['react-hot', 'babel'],
       include: path.join(__dirname, 'src')
+    },
+    {
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel', // 'babel-loader' is also a legal name to reference
+      query: {
+        presets: ['react', 'es2015']
+      }
     }]
   }
 };
